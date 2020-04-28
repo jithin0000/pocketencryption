1f37dee0ed7bea903d5a139c65aad8946cf7a970
jithin0000 <mbjithinbabu@gmail.com>
2020-04-28 12:11:26 +0530

    design almost completed

--- pocketenc/settings.py
+++ pocketenc/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
+EMAIL_PORT = 587
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
